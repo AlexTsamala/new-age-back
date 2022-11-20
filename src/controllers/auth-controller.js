f0ed7b408f0fec408a72f7e337af459decf64c7d
Alex <s.tsamalashvili1998@gmail.com>
2022-11-20 16:38:50 +0400

    added logIn route and jwtSecretKey

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,7 +2,10 @@ import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from 'uuid';
 import pool from "../config/postgres.js";
 import authValidation from "../schemas/auth-validation-shema.js";
+import dotenv from "dotenv";
+import  jwt  from "jsonwebtoken";
 
+dotenv.config();
 
 export const createUser = async(request,response) => {
 
@@ -25,3 +28,32 @@ export const createUser = async(request,response) => {
 
     return response.status(201).send("User was created successfully");
 }
+
+export const logIn = async (request,response) => {
+    const {email,password} = request.body;
+    const hashedPassword = await pool.query({
+      text:`SELECT password FROM "Users" WHERE email='${email}'`,
+    })
+    const checkedPassword =await bcrypt.compare(password,hashedPassword.rowCount > 0 ? hashedPassword.rows[0].password : "");
+    if(!checkedPassword){
+      return response.status(400).json("Password or email is incorrect");
+    }
+    const userInfo = await pool.query({
+        text:`SELECT * FROM "Users" WHERE email='${email}'`,
+    })
+    const {firstName,lastName,id,birthday} = userInfo.rows[0];
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let payload = {
+        "firstName":firstName,
+        "lastName":lastName,
+        "id":id,
+        "email":email,
+        "birthday":birthday
+    }
+    const token = jwt.sign(payload,jwtSecretKey);
+    return response.status(201).json( {
+        token,
+        expireDate: new Date(),
+      })
+}
+
--- src/routes/account-router.js
+++ src/routes/account-router.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/auth-controller.js";
+import { createUser,logIn } from "../controllers/auth-controller.js";
 
 const accountRouter = express.Router(); 
 
 accountRouter.post("/auth/sign-up",createUser);
-// accountRouter.post("/auth/sign-in",ChangePassword);
+accountRouter.post("/auth/sign-in",logIn);
 // accountRouter.get("/Accounts/photo/:userName",usersPhoto);
 
 
